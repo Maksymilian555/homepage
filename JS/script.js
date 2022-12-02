f9b6d5af51711d4fb10865636b13e397dfae7b8b
Maksymilian Gozdek <gozdek.maks@gmail.com>
2022-12-02 11:37:51 +0100

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+let euroElement = document.querySelector(".js-sum");
+let courseElement = document.querySelector(".js-course");
+let formElement = document.querySelector(".js-form");
+let resultElement = document.querySelector(".js-zl");
+let euroResultElement = document.querySelector(".js-eur");
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let euro = euroElement.value;
+    let course = courseElement.value;
+
+    let zl = euro * course;
+
+    resultElement.innerText = zl;
+    euroResultElement.innerText = euro;
+});
+
